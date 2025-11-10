f0b8ff29c055a1c8a2c0286ce96e53dcf6eb20b6
Diamond10011 <np05cp4a230169@iic.edu.np>
2025-11-10 12:15:25 +0545

    all task complete

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter a number N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int sum1 = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            sum1 += i;
+        }
+
+        Console.WriteLine("Sum from 1 to " + N + " = " + sum1);
+        
+        
+        Console.WriteLine("\nNumbers from 1 to 20 (skip multiples of 4, stop at 15):");
+        int j = 1;
+
+        while (j <= 20)
+        {
+            if (j == 15)
+                break; // stop the loop
+
+            if (j % 4 == 0)
+            {
+                j++;
+                continue; // skip multiples of 4
+            }
+
+            Console.WriteLine(j);
+            j++;
+        }
+
+        
+        Console.WriteLine("\nSum of array elements:");
+        int[] numbers = {1, 5, 8, 3, 10, 2 };
+        int sum2 = 0;
+
+        foreach (int num in numbers)
+        {
+            sum2 += num;
+        }
+
+        Console.WriteLine("Sum of array = " + sum2);
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.Write("Enter a number: ");
+        try
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number format");
+        }
+        finally
+        {
+            Console.WriteLine("Program Executed");
+        }
+
+        try
+        {
+            
+        Console.Write("\nEnter a password: ");
+        string password = Console.ReadLine();
+
+        try
+        {
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+
+            Console.WriteLine("Password satisfied the requirement");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("An error occurred while processing the password.");
+        }
+    }
+}
